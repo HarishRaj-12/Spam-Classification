efa56d9e15b8278a1c7916b04a061fada6c35356
HARISH RAJ B <97335089+HarishRaj-12@users.noreply.github.com>
2024-03-07 12:09:58 +0530

    Add files via upload

---
+++ Spam_deployed_mod.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import tensorflow_text
+import math
+from tensorflow.keras.models import load_model
+
+st.set_page_config(page_title="Spam Classification", page_icon=":tada:")
+
+def load_spam_model(model_path):
+    with st.spinner("Loading model..."):
+        loaded_model = load_model(model_path)
+    return loaded_model
+
+def predict_spam(text, model):
+    score = model.predict([text])[0]
+    return score
+
+def main():
+    st.title("Spam Classification")
+    model_path = "C:/Users/haris/Desktop/ML/Spam Classification/model"
+    loaded_model = load_spam_model(model_path)
+
+    text_input = st.text_input("Enter the Text").lower()
+    st.write("---")
+    
+    if st.button('PREDICT'):
+        with st.spinner("Please wait..."):
+            score = predict_spam(text_input, loaded_model)
+        if score < 0.5:
+            st.subheader("Ham")
+        else:
+            st.subheader("Spam")
+
+if __name__ == "__main__":
+    main()
